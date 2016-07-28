d174b57b176a10160a89fcccdeb4c939af52ae04
leoneleone <khalidlaith@hotmail.com>
2016-07-28 16:33:47 +0100

    Added Fibaro floodSensor
    Added s.type == "com.fibaro.floodSensor"
    
                			else if (s.type == "com.fibaro.FGFS101" || s.type == "com.fibaro.floodSensor")
                		service = {controlService: new Service.LeakSensor(s.name), characteristics: [Characteristic.LeakDetected]};

--- index.js
+++ index.js
@@ -166,7 +166,7 @@ FibaroHC2Platform.prototype = {
             		service = {controlService: new Service.ContactSensor(s.name), characteristics: [Characteristic.ContactSensorState]};
 				else if (s.type == "com.fibaro.lightSensor")
             		service = {controlService: new Service.LightSensor(s.name), characteristics: [Characteristic.CurrentAmbientLightLevel]};
-                        else if (s.type == "com.fibaro.FGFS101" || s.type == "com.fibaro.floodSensor")
+            			else if (s.type == "com.fibaro.FGFS101" || s.type == "com.fibaro.floodSensor")
             		service = {controlService: new Service.LeakSensor(s.name), characteristics: [Characteristic.LeakDetected]};
             	else if (s.type == "com.fibaro.FGWP101")
             		service = {controlService: new Service.Outlet(s.name), characteristics: [Characteristic.On, Characteristic.OutletInUse]};
